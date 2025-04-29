b91234fa87e467bbf59f09101a955b0a0ef5f545
LorenaBastos <lorenabastos066@gmail.com>
2025-04-29 14:11:33 -0300

    chore: add variables and second machine

--- output.tf
+++ output.tf
@@ -2,3 +2,8 @@ output "instance_id" {
   description = "ID of the EC2 instance"
   value       = aws_instance.webserver.id
 }
+
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.api-server.id
+}
