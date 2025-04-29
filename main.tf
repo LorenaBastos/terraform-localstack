074243d985b609c44dbeba0151ebece02baf35aa
LorenaBastos <lorenabastos066@gmail.com>
2025-04-29 14:52:48 -0300

    chore: adjust modules

--- main.tf
+++ main.tf
@@ -1,14 +1,11 @@
 module "dev_module" {
- source  = "../dev"
- version = "1.0.0"
+ source  = "./dev"
 }
 
 module "qa_module" {
- source  = "../qa"
- version = "1.0.0"
+ source  = "./qa"
 }
 
 module "prod_module" {
- source  = "../prod"
- version = "1.0.0"
+ source  = "./prod"
 }
